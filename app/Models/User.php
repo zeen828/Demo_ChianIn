cdefb3c0ea00612acfb979597f258e2132cdb16c
zeen828 <zeren828@gmail.com>
2026-05-15 13:56:15 +0800

    安裝Filament Shield套件

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
