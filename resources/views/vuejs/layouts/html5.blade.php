dbd4c1d369e1d860222e0a977be838525fe466f3
zeen828 <zeren828@gmail.com>
2026-06-05 17:30:00 +0800

    調整替換區

--- resources/views/vuejs/layouts/html5.blade.php
+++ resources/views/vuejs/layouts/html5.blade.php
@@ -10,14 +10,46 @@
         @include('vuejs.layouts.style')
     </head>
     <body>
-        <div id="app" class="container">
+        <div id="app" class="container-xl">
             @include('vuejs.partials.header')
+
+@section('content')
+            <div id="carouselExampleIndicators" class="carousel slide ratio ratio-4x3" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="/images/demo/puu.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="/images/demo/puu.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="/images/demo/puu.jpg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+            @include('vuejs.partials.navbar')
+
             <h1>@{{ message }}</h1>
 
             <button @click="count++">
                 點擊次數：@{{ count }}
             </button>
-@yield('content')
+@show
+
             @include('vuejs.partials.footer')
         </div>
         @include('vuejs.layouts.scripts')
--- resources/views/vuejs/pages/home.blade.php
+++ resources/views/vuejs/pages/home.blade.php
@@ -3,9 +3,7 @@
 @section('title', '首頁')
 
 @section('content')
-            <h1>首頁</h1>
-
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div id="carouselExampleIndicators" class="carousel slide ratio ratio-4x3" data-bs-ride="carousel">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -31,4 +29,13 @@
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
+
+            @include('vuejs.partials.navbar')
+
+            <h1>@{{ message }}</h1>
+
+            <button @click="count++">
+                點擊次數：@{{ count }}
+            </button>
+
 @endsection
