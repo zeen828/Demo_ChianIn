fe372b86cdb45e2f2295e0b728c33040328c3368
zeen828 <zeren828@gmail.com>
2026-06-22 17:07:02 +0800

    樣式調整紀錄

--- resources/views/vuejs/layouts/style.blade.php
+++ resources/views/vuejs/layouts/style.blade.php
@@ -1,6 +1,8 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" rel="stylesheet">
         <link href="{{ asset('/css/filament/filament/custom/style_2.css') }}" rel="stylesheet">
+        <style>
+        </style>
 @section('style_custom')
         <style>
         </style>
